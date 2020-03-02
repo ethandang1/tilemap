7157af63b9b4d1e17817037357e951b370fef21d
ethandang1 <58610660+ethandang1@users.noreply.github.com>
2020-03-02 13:02:52 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,104 @@
+namespace myTiles {
+    //% blockIdentity=images._tile
+    export const tile0 = img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`
+}
+function buildLevel () {
+    if (nextLevel == levels.length) {
+        game.over(true)
+    }
+    scene.setTileMap(levels[nextLevel])
+    for (let value of scene.getTilesByType(4)) {
+        value.place(player1)
+    }
+    nextLevel += 1
+}
+scene.onHitTile(SpriteKind.Player, 7, function (sprite) {
+    buildLevel()
+})
+let player1: Sprite = null
+let nextLevel = 0
+let levels: Image[] = []
+levels = [img`
+f f f f f f f f f f 
+f f f f f f f f f f 
+f f f f f f f f f f 
+f 4 f f f f f f 7 f 
+f f f f f f f f f f 
+f f f f f f f f f f 
+f f f f f f f f f f 
+f f f f f f f f f f 
+`, img`
+9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 
+9 4 9 9 9 9 9 9 7 9 
+9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 
+`, img`
+3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 
+3 4 3 3 3 3 3 3 7 3 
+3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 
+`]
+scene.setTile(7, img`
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 f 7 7 7 7 f 7 7 7 7 7 7 
+7 7 7 7 f 7 7 7 7 f 7 7 7 7 7 7 
+7 7 7 7 f 7 7 7 7 f 7 7 7 7 7 7 
+7 7 7 7 f 7 7 7 7 f 7 7 7 7 7 7 
+7 7 7 7 f 7 7 7 7 f 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 f 7 7 7 7 7 7 7 7 f f 7 7 7 
+7 7 7 f f 7 7 7 7 7 7 f 7 7 7 7 
+7 7 7 7 f f 7 7 7 7 f f 7 7 7 7 
+7 7 7 7 7 f f f f f f 7 7 7 7 7 
+7 7 7 7 7 7 7 f f 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+`, true)
+nextLevel = 0
+player1 = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . 2 2 2 2 2 . . . . . . . . 
+. . 2 2 . . . 2 2 . . . . . . . 
+. . 2 . 7 . 7 . 2 . . . . . . . 
+. . 2 2 . . . 2 2 . . . . . . . 
+. . . 2 2 2 2 2 . . . . . . . . 
+. . . . . 2 . . . . . . . . . . 
+. . . . . 2 . . . . . . . . . . 
+. . . 2 2 2 2 2 . . . . . . . . 
+. . . . . 2 . . . . . . . . . . 
+. . . . . 2 . . . . . . . . . . 
+. . . . 2 2 2 . . . . . . . . . 
+. . . 2 2 . 2 2 . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, SpriteKind.Player)
+controller.moveSprite(player1, 100, 100)
+buildLevel()
